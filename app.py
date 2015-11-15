92a9741f47a11ceaeea57ef49040df622f0a9263
chrisbeard <christopher.e.beard@gmail.com>
2015-11-15 12:28:29 -0500

    Charts working, updates live

--- app.py
+++ app.py
@@ -18,12 +18,14 @@ votes = db.votes
 
 # dummy user
 userId = '5647efffa4c06b853bb97d9a'
+# userId = '5648bbd09c5238a2004f926f'
 userId = bson.objectid.ObjectId(userId)
-# print o
 
 # root view
 @app.route("/", methods=['GET', 'POST'])
 def hello():
+    for u in users.find():
+        print u
     if request.method == 'GET':
         q = [q for q in questions.find()]
         v = [v for v in votes.find({'user': userId})]
@@ -63,7 +65,6 @@ def display_poll(poll_id):
     poll_id = bson.objectid.ObjectId(poll_id)
     q = [q for q in questions.find({'_id': poll_id})]
     v = [v for v in votes.find({'user': userId, 'question' : poll_id })]
-    print v[0]
     for qq in q:
         qq['vote'] = None
         for vv in v:
@@ -78,28 +79,79 @@ def poll_stats(poll_id):
     #poll = question!!
     poll_id = bson.objectid.ObjectId(poll_id)
     q = [q for q in questions.find({'_id': poll_id})]
-    v = [v for v in votes.find({'user': userId, 'question' : poll_id })]
+    v = [v for v in votes.find({'question' : poll_id })]
     u = [u for u in users.find({})]
     u_dict = {}
     for user in u:
-      u_dict[user["_id"]] = user
+        u_dict[user["_id"]] = user
 
     d = {}
-    d["n_yes"] = d["n_no"] = d["n_women_yes"] = d["n_men_yes"] = \
-        d["n_women_no"] = d["n_men_no"] = 0
+    d["n_women_yes"] = d["n_men_yes"] = d["n_women_no"] = d["n_men_no"] = 0
+    ay = {}
+    an = {}
+    ay["1-10"] = ay["11-20"] = ay["21-30"] = ay["31-40"] = ay["41-50"] = \
+        ay["51-60"] = ay["61-70"] = ay["71-80"] = ay["81-90"] = ay["91-100"] = 0
+    an["1-10"] = an["11-20"] = an["21-30"] = an["31-40"] = an["41-50"] = \
+        an["51-60"] = an["61-70"] = an["71-80"] = an["81-90"] = an["91-100"] = 0
+    d["age-yes"] = ay
+    d["age-no"] = an
     for vote in v:
-      #0=no, 1=yes
-      gender = u_dict[vote["user"]]["gender"]
-      if gender == 0: #boi
-        if (vote["answer"] == 0):
-          d["n_men_no"] += 1
+        #0=no, 1=yes
+        gender = u_dict[vote["user"]]["gender"]
+        if gender == 0: #boi
+            if (vote["answer"] == 0):
+                d["n_men_no"] += 1
+            else:
+                d["n_men_yes"] += 1
+        elif gender == 1: #gurl
+            if (vote["answer"] == 0):
+                d["n_women_no"] += 1
+            else:
+                d["n_women_yes"] += 1
+
+        age = int(u_dict[vote["user"]]["age"])
+        if (vote["answer"] == 1):
+            if age < 11:
+                ay["1-10"] += 1
+            elif age < 21:
+                ay["11-20"] += 1
+            elif age < 31:
+                ay["21-30"] += 1
+            elif age < 41:
+                ay["31-40"] += 1
+            elif age < 51:
+                ay["41-50"] += 1
+            elif age < 61:
+                ay["51-60"] += 1
+            elif age < 71:
+                ay["61-70"] += 1
+            elif age < 81:
+                ay["71-80"] += 1
+            elif age < 91:
+                ay["81-90"] += 1
+            else:
+                ay["91-100"] += 1
         else:
-          d["n_men_yes"] += 1
-      elif gender == 1: #gurl
-        if (vote["answer"] == 0):
-          d["n_women_no"] += 1
-        else:
-          d["n_women_yes"] += 1
+            if age < 11:
+                an["1-10"] += 1
+            elif age < 21:
+                an["11-20"] += 1
+            elif age < 31:
+                an["21-30"] += 1
+            elif age < 41:
+                an["31-40"] += 1
+            elif age < 51:
+                an["41-50"] += 1
+            elif age < 61:
+                an["51-60"] += 1
+            elif age < 71:
+                an["61-70"] += 1
+            elif age < 81:
+                an["71-80"] += 1
+            elif age < 91:
+                an["81-90"] += 1
+            else:
+                an["91-100"] += 1
 
     return json.dumps(d)
 
@@ -107,17 +159,10 @@ def poll_stats(poll_id):
 @app.route("/poll/<string:poll_id>", methods=["POST", "OPTIONS"])
 @headers({'Access-Control-Allow-Origin':'*'})
 def cast_vote(poll_id):
-    for v in votes.find():
-        print v
     poll_id, vote = poll_id.split("-")
     poll_id = bson.objectid.ObjectId(poll_id)
-    print poll_id, vote
     result = votes.update_one({'user': userId, 'question' : poll_id}, {"$set": {'answer' : float(vote)}}, upsert=True)
-    print result.matched_count
-    print result.modified_count
     return "abc"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
-    #poll_stats("5647f050a4c06b853bb97d9d")
-
